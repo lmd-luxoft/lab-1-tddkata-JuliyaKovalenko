b257471a9cc09564c3d160f3d6f88551e76232aa
JuliyaKovalenko <58985551+JuliyaKovalenko@users.noreply.github.com>
2020-12-07 20:32:17 +0200

    step3: 2 separators, no limited numbers

---
+++ VS2017/KATA2017/KATA2017/StringCalc.cs
@@ -0,0 +1,41 @@
+﻿// NUnit 3 tests
+// See documentation : https://github.com/nunit/docs/wiki/NUnit-Documentation
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TDDKata
+{
+    internal class StringCalc
+    {
+        internal int Sum(string v)
+        {
+
+            int Sum = 0;
+            
+            string[] s = new string[] { ",", "\n" };
+
+           
+            //int[] numbers = v.Split(',').Select(int.Parse).ToArray();
+
+            int[] numbers = v.Split(s, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+
+
+            //if (numbers.Count() > 2)
+            //    return -1;
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] < 0)
+                    return -1;
+            }
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Sum += numbers[i];
+
+            }
+            return Sum;
+        }
+    }
+}
---
+++ VS2017/KATA2017/KATA2017/TestClass.cs
@@ -0,0 +1,76 @@
+﻿// NUnit 3 tests
+// See documentation : https://github.com/nunit/docs/wiki/NUnit-Documentation
+using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace TDDKata
+{
+    [TestFixture]
+    public class TestClass
+    {
+        [Test]
+        public void SimpleTest()
+        {
+            StringCalc calc = new StringCalc();
+            int value = calc.Sum("2,2");
+            Assert.That(value, Is.EqualTo(4), "Wrong actual value");
+        }
+
+        [Test]
+        public void SumShouldBeCorrect()
+        {
+            StringCalc stringCalc = new StringCalc();
+            string str = "0,1";
+            int expected = 1;
+
+            int actual = stringCalc.Sum(str);
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+
+        [Test]
+        public void SumShouldBeStringIsEmpty()
+        {
+            StringCalc stringCalc = new StringCalc();
+            string str = "";
+            int expected = 0;
+            int actual = str.Length;
+            Assert.That(actual, Is.EqualTo(expected));
+
+        }
+
+        [Test]
+        public void SumShouldBeNotHaveNegative()
+        {
+            StringCalc stringCalc = new StringCalc();
+            string str = "-1,2,3";
+            int expected = -1;
+            int actual = stringCalc.Sum(str);
+
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+
+        //[Test]
+        //public void SumShoulHaveLessThan3Nubers()
+        //{
+        //    StringCalc stringCalc = new StringCalc();
+        //    string str = "1,2,3,4,5";
+        //    int expected = -1;
+        //    int actual = stringCalc.Sum(str);
+
+        //    Assert.That(actual, Is.EqualTo(expected));
+        //}
+
+        [Test]
+        public void SumCanHaveDifferents2LSeparators()
+        {
+            StringCalc stringCalc = new StringCalc();
+            string str = "1,4\n5";
+            int expected = 10;
+            int actual = stringCalc.Sum(str);
+
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+
+    }
+}
