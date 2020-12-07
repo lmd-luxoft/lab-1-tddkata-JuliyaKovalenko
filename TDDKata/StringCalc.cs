00cb816f99c2814e1833a88f61016cbdb7f55091
JuliyaKovalenko <58985551+JuliyaKovalenko@users.noreply.github.com>
2020-12-07 20:26:28 +0200

    step2 into KATAVS2017

--- TDDKata/StringCalc.cs
+++ TDDKata/StringCalc.cs
@@ -1,6 +1,7 @@
 ﻿// NUnit 3 tests
 // See documentation : https://github.com/nunit/docs/wiki/NUnit-Documentation
 using System;
+using System.Collections.Generic;
 
 namespace TDDKata
 {
@@ -8,7 +9,15 @@ namespace TDDKata
     {
         internal int Sum(string v)
         {
-            throw new NotImplementedException();
+            string v = "1,2,3";
+            int Sum = 0;
+            string[] strArr = v.Split(",");
+            int[] numArr = new int[strArr.length];
+            for (int i = 0; i < numArr.length; i++)
+            {
+                Sum += numArr[i];
+
+            }
         }
     }
 }
